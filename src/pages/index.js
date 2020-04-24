f30dc23daa0a4f6c0f1a5b2a44ea738e323e3940
Rik Lenon <riklenon@gmail.com>
2020-04-24 01:23:40 -0300

    Ferr

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,14 +8,16 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Olá mortal</h1>
+    <p>BEM VINDO AO SITE MAIS ESCARALHADO DA INTERWEBS.</p>
+    <p>CHEIRO DE PNEU QUEIMADO, CARBURADOR FURADO, O x9 FOI TORRADO.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    
+    <Link to="/page-2">Go to page 2</Link>
   </Layout>
+  
 )
 
 export default IndexPage
